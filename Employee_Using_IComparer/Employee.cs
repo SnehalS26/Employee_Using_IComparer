7df546f790f96983c5909059a00b5419d724a56f
SnehalS26 <Snehalshirsath26@gmail.com>
2023-07-11 17:40:30 +0530

    Add project files.

---
+++ Employee_Using_IComparer/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Using_IComparer
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+
+    public class SalaryComaprision : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            if(x.Salary < y.Salary)
+            {
+                return -1;
+            }
+            else if(x.Salary > y.Salary)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Employee_Using_IComparer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Using_IComparer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee{ Name = "Snehal" , Salary = 20000};
+            Employee emp1 = new Employee { Name = "Sneh", Salary = 15000 };
+
+            SalaryComaprision sal = new SalaryComaprision();
+            int result = sal.Compare(emp, emp1);
+
+            if(result<0)
+            {
+                Console.WriteLine("Snehal has less salary than sneh");
+            }
+            else if(result>0)
+            {
+                Console.WriteLine("Snehal has more salary than sneh");
+            }
+            else
+            {
+                Console.WriteLine("Snehal & sneh has same salary");
+            }
+        }
+    }
+}
